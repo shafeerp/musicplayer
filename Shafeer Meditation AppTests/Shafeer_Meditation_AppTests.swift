dad2fbcfcbb128725da4ed8e386d26049c7f0d50
Shafeer Puthalan <shafeer.puthalan@cowrks.com>
2019-04-14 15:43:16 +0530

    Initial Commit

---
+++ Shafeer Meditation AppTests/Shafeer_Meditation_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Shafeer_Meditation_AppTests.swift
+//  Shafeer Meditation AppTests
+//
+//  Created by Shafeer Puthalan on 14/04/19.
+//  Copyright © 2019 Shafeer Puthalan. All rights reserved.
+//
+
+import XCTest
+@testable import Shafeer_Meditation_App
+
+class Shafeer_Meditation_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
